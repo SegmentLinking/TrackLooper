45d71e7939a7f3db546b941441ef01c5d147e346
Philip Chang <sgnoohc@gmail.com>
2020-12-21 10:52:32 -0800

    adding a feature to remake binaries

--- make_script.sh
+++ make_script.sh
@@ -22,16 +22,18 @@ usage()
   echo "  -x    explicit memory        (Make library with explicit memory enabled)"
   echo "  -c    cache                  (Make library with cache enabled)"
   echo "  -s    show log               (Full compilation script to stdout)"
+  echo "  -m    make clean binaries    (Make clean binaries before remake. e.g. when header files changed in SDL/*.cuh)"
   echo
   exit
 }
 
 # Parsing command-line opts
-while getopts ":cxsh" OPTION; do
+while getopts ":cxsmh" OPTION; do
   case $OPTION in
     c) MAKECACHE=true;;
     x) MAKEEXPLICIT=true;;
     s) SHOWLOG=true;;
+    m) MAKECLEANBINARIES=true;;
     h) usage;;
     :) usage;;
   esac
@@ -41,6 +43,7 @@ done
 if [ -z ${MAKECACHE} ]; then MAKECACHE=false; fi
 if [ -z ${MAKEEXPLICIT}  ]; then MAKEEXPLICIT=false; fi
 if [ -z ${SHOWLOG} ]; then SHOWLOG=false; fi
+if [ -z ${MAKECLEANBINARIES} ]; then MAKECLEANBINARIES=false; fi
 
 # Shift away the parsed options
 shift $(($OPTIND - 1))
@@ -55,9 +58,10 @@ echo "Line Segment Tracking Compilation Script             "  | tee -a ${LOG}
 echo "====================================================="  | tee -a ${LOG}
 echo "Compilation options set to..."                          | tee -a ${LOG}
 echo ""                                                       | tee -a ${LOG}
-echo "  MAKECACHE      : ${MAKECACHE}"                        | tee -a ${LOG}
-echo "  MAKEEXPLICIT   : ${MAKEEXPLICIT}"                     | tee -a ${LOG}
-echo "  SHOWLOG        : ${SHOWLOG}"                          | tee -a ${LOG}
+echo "  MAKECACHE         : ${MAKECACHE}"                     | tee -a ${LOG}
+echo "  MAKEEXPLICIT      : ${MAKEEXPLICIT}"                  | tee -a ${LOG}
+echo "  SHOWLOG           : ${SHOWLOG}"                       | tee -a ${LOG}
+echo "  MAKECLEANBINARIES : ${MAKECLEANBINARIES}"             | tee -a ${LOG}
 echo ""                                                       | tee -a ${LOG}
 echo "  (cf. Run > sh $(basename $0) -h to see all options)"  | tee -a ${LOG}
 echo ""                                                       | tee -a ${LOG}
@@ -70,6 +74,18 @@ if $MAKEEXPLICIT; then MAKETARGET=explicit; fi
 
 # If make cache is true then make library with cache enabled
 if $MAKECACHE; then MAKETARGET=${MAKETARGET}_cache; fi
+
+# If make clean binaries are called then first make clean before making
+if $MAKECLEANBINARIES; then
+    echo "First make cleaning all of TrackLooper objects and sdl.so" | tee -a ${LOG}
+    make clean >> ${LOG} 2>&1
+fi
+
+###
+###
+### Making Line Segment Tracking Library
+###
+###
 
 echo "Line Segment Tracking GPU library with MAKETARGET=${MAKETARGET} is being compiled...." | tee -a ${LOG}
 
@@ -88,9 +104,18 @@ if [ ! -f SDL/sdl.so ]; then
     exit
 fi
 
+echo "" >> ${LOG}
+echo "" >> ${LOG}
+echo "" >> ${LOG}
 echo "Line Segment Tracking GPU library compilation with MAKETARGET=${MAKETARGET} successful!" | tee -a ${LOG}
-
 echo "" | tee -a ${LOG}
+
+###
+###
+### Making Line Segment Tracking Binaries
+###
+###
+
 echo "Line Segment Tracking binaries are being compiled...." | tee -a ${LOG}
 
 echo "---------------------------------------------------------------------------------------------" >> ${LOG} 2>&1 
@@ -119,4 +144,5 @@ echo "" >> ${LOG}
 echo "" >> ${LOG}
 echo "Line Segment Tracking binaries compilation successful!" | tee -a ${LOG}
 echo "" | tee -a ${LOG}
+
 echo "Compilation is logged at ${LOG}" | tee -a ${LOG}
