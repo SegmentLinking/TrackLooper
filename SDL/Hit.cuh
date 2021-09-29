9d4bbd20cae232b230be678fe148881c80ba0738
Bei Wang <beiwang@lnx7188.classe.cornell.edu>
2021-09-29 15:02:13 -0400

    miss one file from last commit

--- SDL/Hit.cuh
+++ SDL/Hit.cuh
@@ -56,11 +56,39 @@ namespace SDL
     //CUDA_G void checkHits(struct hits& hitsInGPU, const int loopsize);
     void addHitToMemory(struct hits& hitsInCPU,struct modules& modulesInGPU,float x, float y, float z, unsigned int detId, unsigned int idxInNtuple);
     CUDA_G void addHitToMemoryGPU(struct hits& hitsInCPU,struct modules& modulesInGPU,float x, float y, float z, unsigned int detId, unsigned int idxInNtuple,unsigned int moduleIndex, float phis);
-    CUDA_HOSTDEV float phi(float x, float y, float z);
-    CUDA_HOSTDEV float ATan2(float y, float x);
-    CUDA_HOSTDEV float phi_mpi_pi(float phi);
-    CUDA_HOSTDEV float deltaPhi(float x1, float y1, float z1, float x2, float y2, float z2);
-    CUDA_HOSTDEV float deltaPhiChange(float x1, float y1, float z1, float x2, float y2, float z2);
+    
+    CUDA_HOSTDEV inline float ATan2(float y, float x) {
+    if (x != 0) return  atan2f(y, x);
+    if (y == 0) return  0;
+    if (y >  0) return  M_PI / 2;
+    else        return -M_PI / 2;
+    }
+    CUDA_HOSTDEV inline float phi_mpi_pi(float x) {
+    if (isnan(x))
+    {
+      //printf("phi_mpi_pi() function called with NaN\n");                                                
+        return x;
+    }
+
+    while (x >= M_PI)
+        x -= 2. * M_PI;
+
+    while (x < -M_PI)
+        x += 2. * M_PI;
+
+    return x;
+    }
+    CUDA_HOSTDEV inline float phi(float x, float y, float z) {
+        return phi_mpi_pi(M_PI + ATan2(-y, -x));
+    }
+    CUDA_HOSTDEV inline float deltaPhi(float x1, float y1, float z1, float x2, float y2, float z2) {
+    float phi1 = phi(x1,y1,z1);
+    float phi2 = phi(x2,y2,z2);
+    return phi_mpi_pi((phi2 - phi1));
+    }
+    CUDA_HOSTDEV inline float deltaPhiChange(float x1, float y1, float z1, float x2, float y2, float z2) {
+    return deltaPhi(x1,y1,z1,x2-x1, y2-y1, z2-z1);
+    }
     void getEdgeHits(unsigned int detId,float x, float y, float& xhigh, float& yhigh, float& xlow, float& ylow);
     CUDA_DEV void getEdgeHitsK(float phi,float x, float y, float& xhigh, float& yhigh, float& xlow, float& ylow);
 
