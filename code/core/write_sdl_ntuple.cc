fe66c2dfd45416d7eb78b27aa0e3d386e7d80db2
Tres Reid <tres.reid@gmail.com>
2021-11-19 09:56:40 -0500

    update timing printout

--- code/core/write_sdl_ntuple.cc
+++ code/core/write_sdl_ntuple.cc
@@ -4754,7 +4754,7 @@ void printTimingInformation(std::vector<std::vector<float>>& timing_information,
     //float std_throughput = effectiveThroughput * sqrt(pow(stdDevFull/timing_total_avg,2) + pow(stdDev/timing_totalshort_avg,2));
 
     std::cout << setprecision(0);
-    std::cout << "   Evt    Hits       MD       LS      T3       TC       T5       pT3      pT5      pLS      Kernel     Kernel(short)    Loop      throughput" << std::endl;
+    std::cout << "   Evt    Hits       MD       LS      T3       TC       T5       pT3      pT5      pLS      Kernel     Kernel(short)     Loop      Effective" << std::endl;
     std::cout << setw(6) << "avg";
     std::cout << "   "<<setw(6) << timing_sum_information[0]; // Hits
     std::cout << "   "<<setw(6) << timing_sum_information[1]; // MD
@@ -4768,7 +4768,7 @@ void printTimingInformation(std::vector<std::vector<float>>& timing_information,
     std::cout << "   "<<setw(7) << timing_total_avg; // Average total time
     //std::cout << "+/- "<< stdDevFull;
     std::cout << "   "<<setw(7) << timing_totalshort_avg; // Average total time
-    std::cout << "+/- "<< stdDev; 
+    std::cout << "+/- "<< setw(4)<<stdDev; 
     //std::cout << "   "<<setw(7) << fullTime; // Full time
     std::cout << "   "<<setw(7) << fullavg; // Average full time
     std::cout << "   "<<setw(7) << effectiveThroughput; // Effective time
