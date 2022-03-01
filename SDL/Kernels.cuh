2d1651d842cdb8f74fbd5c73ca8c3ad3da9af717
YonsiG <yag002@ucsd.edu>
2022-03-01 00:07:44 -0500

    change t3 and t5 module occupancy to 99.99%

--- SDL/Kernels.cuh
+++ SDL/Kernels.cuh
@@ -43,7 +43,7 @@ const unsigned int N_MAX_PIXEL_SEGMENTS_PER_MODULE = 50000;
 
 
 const unsigned int N_MAX_TRIPLETS_PER_MODULE = 1170;
-const unsigned int N_MAX_QUINTUPLETS_PER_MODULE = 1026;
+const unsigned int N_MAX_QUINTUPLETS_PER_MODULE = 513;
 const unsigned int N_MAX_PIXEL_TRIPLETS = 5000;
 const unsigned int N_MAX_PIXEL_QUINTUPLETS = 15000;
 const unsigned int N_MAX_TOTAL_TRIPLETS = 200000;
