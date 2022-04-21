7ec2ae7c32df9d4d3cab494a31963649d3f19c6d
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2022-04-21 16:45:03 -0400

    Comment out functions using the missing branches (simhit_p*) in CMSSW12 geometry ntuples

--- code/core/trkCore.cc
+++ code/core/trkCore.cc
@@ -1,126 +1,123 @@
-#include "trkCore.h"
-
-//__________________________________________________________________________________________
-float simhit_p(unsigned int simhitidx)
-{
-
-    // |momentum| calculation
-
-    float px = trk.simhit_px()[simhitidx];
-    float py = trk.simhit_py()[simhitidx];
-    float pz = trk.simhit_pz()[simhitidx];
-    return sqrt(px*px + py*py + pz*pz);
-}
-
-//__________________________________________________________________________________________
-float hitAngle(unsigned int simhitidx)
-{
-
-    // This is the angle calculation between position vector and the momentum vector
-
-    float x = trk.simhit_x()[simhitidx];
-    float y = trk.simhit_y()[simhitidx];
-    float z = trk.simhit_z()[simhitidx];
-    float r3 = sqrt(x*x + y*y + z*z);
-    float px = trk.simhit_px()[simhitidx];
-    float py = trk.simhit_py()[simhitidx];
-    float pz = trk.simhit_pz()[simhitidx];
-    float p = sqrt(px*px + py*py + pz*pz);
-    float rdotp = x*px + y*py + z*pz;
-    rdotp = rdotp / r3;
-    rdotp = rdotp / p;
-    float angle = acos(rdotp);
-    return angle;
-}
-
-//__________________________________________________________________________________________
-bool isMuonCurlingHit(unsigned int isimtrk, unsigned int ith_hit)
-{
-
-    // To assess whether the ith_hit for isimtrk is a "curling" hit
-
-    // Retrieve the sim hit idx
-    unsigned int simhitidx = trk.sim_simHitIdx()[isimtrk][ith_hit];
-
-    // We're only concerned about hits in the outer tracker
-    // This is more of a sanity check
-    if (not (trk.simhit_subdet()[simhitidx] == 4 or trk.simhit_subdet()[simhitidx] == 5))
-        return false;
-
-    // Retrieve the sim hit pdgId
-    int simhit_particle = trk.simhit_particle()[simhitidx];
-
-    // If the hit is not muon then we can't tell anything
-    if (abs(simhit_particle) != 13)
-        return false;
-
-    // Get the angle
-    float angle = hitAngle(simhitidx);
-
-    // If the angle is quite different then it's the last hit
-    if (abs(angle) > 1.6)
-        return true;
-
-    // Afterwards, we check the energy loss
-    //
-    // If it is the first hit without any previous hit present,
-    // we can't tell whether it is last hit or not
-    // so we just say false to be conservative
-    if (ith_hit == 0)
-        return false;
-
-    // Retrieve the module where the hit lies
-    int detid = trk.simhit_detId()[simhitidx];
-    SDL::CPU::Module module = SDL::CPU::Module(detid);
-
-    // Calculate the momentum
-    float p = simhit_p(simhitidx);
-
-    // Find the previous simhit that is on the lower side of the module
-    int simhitidx_previous = -999;
-    for (unsigned int ith_back = 1; ith_back <= ith_hit; ith_back++)
-    {
-        // Retrieve the hit idx of ith_hit - ith_back;
-        unsigned int simhitidx_previous_candidate = trk.sim_simHitIdx()[isimtrk][ith_hit-ith_back];
-
-        if (not (trk.simhit_subdet()[simhitidx_previous_candidate] == 4 or trk.simhit_subdet()[simhitidx_previous_candidate] == 5))
-            continue;
-
-        if (not (trk.simhit_particle()[simhitidx_previous_candidate] == 13))
-            continue;
-
-        // Retrieve the module where the previous candidate hit lies
-        int detid = trk.simhit_detId()[simhitidx_previous_candidate];
-        SDL::CPU::Module module = SDL::CPU::Module(detid);
-
-        // If the module is lower, then we get the index
-        if (module.isLower())
-        {
-            simhitidx_previous = simhitidx_previous_candidate;
-            break;
-        }
-
-    }
-
-    // If no previous layer is found then can't do much
-    if (simhitidx_previous == -999)
-        return false;
-
-    // Get the previous layer momentum
-    float p_previous = simhit_p(simhitidx_previous);
-
-    // Calculate the momentum loss
-    float loss = fabs(p_previous - p) / p_previous;
-
-    // If the momentum loss is large it is the last hit
-    if (loss > 0.35)
-        return true;
-
-    // If it reaches this point again, we're not sure what this hit is
-    // So we return false
-    return false;
-
-}
+//float simhit_p(unsigned int simhitidx)
+//{
+//
+//    // |momentum| calculation
+//
+//    float px = trk.simhit_px()[simhitidx];
+//    float py = trk.simhit_py()[simhitidx];
+//    float pz = trk.simhit_pz()[simhitidx];
+//    return sqrt(px*px + py*py + pz*pz);
+//}
+//
+////__________________________________________________________________________________________
+//float hitAngle(unsigned int simhitidx)
+//{
+//
+//    // This is the angle calculation between position vector and the momentum vector
+//
+//    float x = trk.simhit_x()[simhitidx];
+//    float y = trk.simhit_y()[simhitidx];
+//    float z = trk.simhit_z()[simhitidx];
+//    float r3 = sqrt(x*x + y*y + z*z);
+//    float px = trk.simhit_px()[simhitidx];
+//    float py = trk.simhit_py()[simhitidx];
+//    float pz = trk.simhit_pz()[simhitidx];
+//    float p = sqrt(px*px + py*py + pz*pz);
+//    float rdotp = x*px + y*py + z*pz;
+//    rdotp = rdotp / r3;
+//    rdotp = rdotp / p;
+//    float angle = acos(rdotp);
+//    return angle;
+//}
+//
+////__________________________________________________________________________________________
+//bool isMuonCurlingHit(unsigned int isimtrk, unsigned int ith_hit)
+//{
+//
+//    // To assess whether the ith_hit for isimtrk is a "curling" hit
+//
+//    // Retrieve the sim hit idx
+//    unsigned int simhitidx = trk.sim_simHitIdx()[isimtrk][ith_hit];
+//
+//    // We're only concerned about hits in the outer tracker
+//    // This is more of a sanity check
+//    if (not (trk.simhit_subdet()[simhitidx] == 4 or trk.simhit_subdet()[simhitidx] == 5))
+//        return false;
+//
+//    // Retrieve the sim hit pdgId
+//    int simhit_particle = trk.simhit_particle()[simhitidx];
+//
+//    // If the hit is not muon then we can't tell anything
+//    if (abs(simhit_particle) != 13)
+//        return false;
+//
+//    // Get the angle
+//    float angle = hitAngle(simhitidx);
+//
+//    // If the angle is quite different then it's the last hit
+//    if (abs(angle) > 1.6)
+//        return true;
+//
+//    // Afterwards, we check the energy loss
+//    //
+//    // If it is the first hit without any previous hit present,
+//    // we can't tell whether it is last hit or not
+//    // so we just say false to be conservative
+//    if (ith_hit == 0)
+//        return false;
+//
+//    // Retrieve the module where the hit lies
+//    int detid = trk.simhit_detId()[simhitidx];
+//    SDL::Module module = SDL::Module(detid);
+//
+//    // Calculate the momentum
+//    float p = simhit_p(simhitidx);
+//
+//    // Find the previous simhit that is on the lower side of the module
+//    int simhitidx_previous = -999;
+//    for (unsigned int ith_back = 1; ith_back <= ith_hit; ith_back++)
+//    {
+//        // Retrieve the hit idx of ith_hit - ith_back;
+//        unsigned int simhitidx_previous_candidate = trk.sim_simHitIdx()[isimtrk][ith_hit-ith_back];
+//
+//        if (not (trk.simhit_subdet()[simhitidx_previous_candidate] == 4 or trk.simhit_subdet()[simhitidx_previous_candidate] == 5))
+//            continue;
+//
+//        if (not (trk.simhit_particle()[simhitidx_previous_candidate] == 13))
+//            continue;
+//
+//        // Retrieve the module where the previous candidate hit lies
+//        int detid = trk.simhit_detId()[simhitidx_previous_candidate];
+//        SDL::Module module = SDL::Module(detid);
+//
+//        // If the module is lower, then we get the index
+//        if (module.isLower())
+//        {
+//            simhitidx_previous = simhitidx_previous_candidate;
+//            break;
+//        }
+//
+//    }
+//
+//    // If no previous layer is found then can't do much
+//    if (simhitidx_previous == -999)
+//        return false;
+//
+//    // Get the previous layer momentum
+//    float p_previous = simhit_p(simhitidx_previous);
+//
+//    // Calculate the momentum loss
+//    float loss = fabs(p_previous - p) / p_previous;
+//
+//    // If the momentum loss is large it is the last hit
+//    if (loss > 0.35)
+//        return true;
+//
+//    // If it reaches this point again, we're not sure what this hit is
+//    // So we return false
+//    return false;
+//
+//}
 
 
 
@@ -147,8 +144,8 @@ bool hasAll12HitsWithNBarrelUsingModuleMap(unsigned int isimtrk, int nbarrel, bo
         if (not (trk.simhit_particle()[simhitidx] == trk.sim_pdgId()[isimtrk]))
             continue;
 
-        if (isMuonCurlingHit(isimtrk, ith_hit))
-            break;
+        //if (isMuonCurlingHit(isimtrk, ith_hit))
+        //    break;
 
         if (not usesimhits)
         {
@@ -296,8 +293,8 @@ bool hasAll12HitsWithNBarrel(unsigned int isimtrk, int nbarrel)
         // if (not (trk.simhit_particle()[simhitidx] == trk.sim_pdgId()[isimtrk]))
         //     continue;
 
-        if (isMuonCurlingHit(isimtrk, ith_hit))
-            break;
+        //if (isMuonCurlingHit(isimtrk, ith_hit))
+        //    break;
 
         // list of reco hit matched to this sim hit
         for (unsigned int irecohit = 0; irecohit < trk.simhit_hitIdx()[simhitidx].size(); ++irecohit)
