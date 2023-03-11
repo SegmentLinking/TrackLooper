8d759aea87db488b4f9e90b2f76daf40397f5f5f
GNiendorf <gavinniendorf@gmail.com>
2023-03-10 23:29:22 -0500

    first working segment kernel to alpaka

--- SDL/Event.cuh
+++ SDL/Event.cuh
@@ -41,7 +41,7 @@ namespace SDL
 
         //CUDA stuff
         int dev;
-        unsigned int nTotalSegments;
+        int nTotalSegments;
         struct objectRanges* rangesInGPU;
         struct hits* hitsInGPU;
         struct miniDoublets* mdsInGPU;
