c6a8accd3316b29df273d7793f4391419267445c
Tres Reid <mgr85@cornell.edu>
2023-04-12 11:39:45 -0400

    Delete moveplots.sh

--- moveplots.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-outdir=test #SegmentLinking_keep/PR178_compv3
-rm -r /cdat/tem/mgr85/www/${outdir}
-cp -r performance/ /cdat/tem/mgr85/www/${outdir}
-#mv performance/ /cdat/tem/mgr85/www/${outdir}
