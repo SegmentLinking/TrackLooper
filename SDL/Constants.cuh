37cd75fe7b705ef16e05b8f462f0407da96d6732
GNiendorf <gavinniendorf@gmail.com>
2023-07-14 12:59:25 -0400

    turn off half precision code

--- SDL/Constants.cuh
+++ SDL/Constants.cuh
@@ -6,56 +6,54 @@
 
 // CUDA headers. Will be removed soon.
 #ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
-#include <cuda.h>
 #include <cuda_fp16.h>
-#include <cuda_runtime.h>
 #endif
 
 //This changes pT5 and pT3 and T3 completely. T5 for non regression parameters
-#if defined(FP16_Base) && defined(ALPAKA_ACC_GPU_CUDA_ENABLED)
-#define __F2H __float2half  
-#define __H2F __half2float  
-typedef __half FPX;
+#if defined(FP16_Base)
+#define __F2H //__float2half  
+#define __H2F //__half2float  
+typedef float FPX;
 #else
 #define __F2H
 #define __H2F
 typedef float FPX; 
 #endif
 
-#if defined(FP16_T5) && defined(ALPAKA_ACC_GPU_CUDA_ENABLED) // changes T5 regression values
-#define __F2H_T5 __float2half  
-#define __H2F_T5 __half2float  
-typedef __half FPX_T5;
+#if defined(FP16_T5) // changes T5 regression values
+#define __F2H_T5 //__float2half  
+#define __H2F_T5 //__half2float  
+typedef float FPX_T5;
 #else
 #define __F2H_T5
 #define __H2F_T5
 typedef float FPX_T5; 
 #endif
 
-#if defined(FP16_dPhi) && defined(ALPAKA_ACC_GPU_CUDA_ENABLED) // changes segment dPhi values
-#define __F2H_dPhi __float2half  
-#define __H2F_dPhi __half2float  
-typedef __half FPX_dPhi;
+#if defined(FP16_dPhi) // changes segment dPhi values
+#define __F2H_dPhi //__float2half  
+#define __H2F_dPhi //__half2float  
+typedef float FPX_dPhi;
 #else
 #define __F2H_dPhi
 #define __H2F_dPhi
 typedef float FPX_dPhi; 
 #endif
 
-#if defined(FP16_circle) && defined(ALPAKA_ACC_GPU_CUDA_ENABLED) // changes segment circle values
-#define __F2H_circle __float2half  
-#define __H2F_circle __half2float  
-typedef __half FPX_circle;
+#if defined(FP16_circle) // changes segment circle values
+#define __F2H_circle //__float2half  
+#define __H2F_circle //__half2float  
+typedef float FPX_circle;
 #else
 #define __F2H_circle
 #define __H2F_circle
 typedef float FPX_circle; 
 #endif
 
-#if defined(FP16_seg) && defined(ALPAKA_ACC_GPU_CUDA_ENABLED)  // changes segment values
-#define __F2H_seg __float2half  
-#define __H2F_seg __half2float  
-typedef __half FPX_seg;
+#if defined(FP16_seg) // changes segment values
+#define __F2H_seg //__float2half  
+#define __H2F_seg //__half2float  
+typedef float FPX_seg;
 #else
 #define __F2H_seg
 #define __H2F_seg
