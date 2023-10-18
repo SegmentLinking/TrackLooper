e5325b60753d8a72a91a15b36282b9a51c4b5336
Andres Rios-Tascon <ariostas@gmail.com>
2023-10-18 08:04:34 -0400

    Fixed overflow when adding pLSs

--- SDL/Segment.h
+++ SDL/Segment.h
@@ -802,7 +802,7 @@ namespace SDL
             unsigned int* hitIndices3,
             float* dPhiChange,
             uint16_t pixelModuleIndex,
-            const int size) const
+            int size) const
         {
             using Dim = alpaka::Dim<TAcc>;
             using Idx = alpaka::Idx<TAcc>;
